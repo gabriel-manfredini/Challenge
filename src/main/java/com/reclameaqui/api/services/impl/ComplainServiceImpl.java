d41206520c9a8a5b7b01d77e1322723875fd798c
Gabriel Manfredini <gabriel.manfredini@gmail.com>
2018-09-18 17:23:42 -0300

    Implemented methods to getAll, getById, Insert and update Complains

--- src/main/java/com/reclameaqui/api/services/impl/ComplainServiceImpl.java
+++ src/main/java/com/reclameaqui/api/services/impl/ComplainServiceImpl.java
@@ -1,57 +1,79 @@
 package com.reclameaqui.api.services.impl;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
 
 import com.reclameaqui.api.documents.Complain;
+import com.reclameaqui.api.repositories.ComplainRepository;
 import com.reclameaqui.api.services.ComplainService;
 
+@Service
 public class ComplainServiceImpl implements ComplainService {
 
+	@Autowired
+	private ComplainRepository complainRepository;
+	
+	
 	@Override
 	public List<Complain> listAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.complainRepository.findAll();
 	}
 
 	@Override
 	public Complain listById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return  this.complainRepository.findOne(id);
 	}
 
 	@Override
 	public List<Complain> listByLocaleAndCompany(String locale, String company) {
-		// TODO Auto-generated method stub
+//		Complain complain = new Complain();
+//		complain.setLocale(locale);
+//		complain.setCompany(company);
+//		
+//		this.complainRepository.
+//		MongoTemplate mongoTemplate = new MongoTemplate();
+//		
+//		return List<User> users = MongoTemplate.find(query, Complain.class);
 		return null;
 	}
 
 	@Override
 	public List<Complain> listByLocale(String locale) {
-		// TODO Auto-generated method stub
+//		Query query = new Query();
+//		query.addCriteria(Criteria.where("locale").is(locale));	
+//		return complainRepository.findAll(query,Complain.class);
 		return null;
 	}
 
 	@Override
 	public List<Complain> listByCompany(String company) {
-		// TODO Auto-generated method stub
+//		Query query = new Query();
+//		query.addCriteria(Criteria.where("company").is(company));	
+//		return MongoTemplate.find(query,Complain.class);
 		return null;
 	}
 
 	@Override
 	public Complain ingest(Complain complain) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.complainRepository.save(complain);
 	}
 
 	@Override
 	public Complain update(Complain complain) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.complainRepository.save(complain);
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
+		this.complainRepository.delete(id);
 
 	}
 
