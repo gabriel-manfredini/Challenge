ee0dfb41b86445db47ffde5eb415fec6494a447b
Gabriel Manfredini <gabriel.manfredini@gmail.com>
2018-09-18 18:25:47 -0300

    Added default response and validation of errors in the Controller

--- src/main/java/com/reclameaqui/api/controllers/ComplainController.java
+++ src/main/java/com/reclameaqui/api/controllers/ComplainController.java
@@ -1,9 +1,13 @@
 package com.reclameaqui.api.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reclameaqui.api.documents.Complain;
+import com.reclameaqui.api.responses.Response;
 import com.reclameaqui.api.services.ComplainService;
 
 @RestController
@@ -24,40 +29,50 @@ public class ComplainController {
 	private ComplainService complainService;
 	
 	@GetMapping
-	public ResponseEntity<List<Complain>> listAll(){
-		return ResponseEntity.ok(this.complainService.listAll());
+	public ResponseEntity<Response<List<Complain>>> listAll(){
+		return ResponseEntity.ok(new Response<List<Complain>>(this.complainService.listAll()));
 	}
 	
 	@GetMapping(path = "/{id}")
-	public ResponseEntity<Complain> listById(@PathVariable(name = "id") String id){
-		return ResponseEntity.ok(this.complainService.listById(id));
+	public ResponseEntity<Response<Complain>> listById(@PathVariable(name = "id") String id){
+		return ResponseEntity.ok(new Response<Complain>(this.complainService.listById(id)));
 	}
 	
 	@GetMapping(path = "/{locale}/{company}")
-	public ResponseEntity<List<Complain>> listByLocaleAndCompany(@PathVariable(name = "locale") String locale, @PathVariable(name = "company") String company){
-		return ResponseEntity.ok(this.complainService.listByLocaleAndCompany(locale, company));
+	public ResponseEntity<Response<List<Complain>>> listByLocaleAndCompany(@PathVariable(name = "locale") String locale, @PathVariable(name = "company") String company){
+		return ResponseEntity.ok(new Response<List<Complain>>(this.complainService.listByLocaleAndCompany(locale, company)));
 	}
 	
 	@GetMapping(path = "/{locale}")
-	public ResponseEntity<List<Complain>> listByLocale(@PathVariable(name = "locale") String locale){
-		return ResponseEntity.ok(this.complainService.listByLocale(locale));
+	public ResponseEntity<Response<List<Complain>>> listByLocale(@PathVariable(name = "locale") String locale){
+		return ResponseEntity.ok(new Response<List<Complain>>(this.complainService.listByLocale(locale)));
 	}
 	
 	@GetMapping(path = "/{company}")
-	public ResponseEntity<List<Complain>> listByCompany(@PathVariable(name = "company") String company){
-		return ResponseEntity.ok(this.complainService.listByCompany(company));
+	public ResponseEntity<Response<List<Complain>>> listByCompany(@PathVariable(name = "company") String company){
+		return ResponseEntity.ok(new Response<List<Complain>>(this.complainService.listByCompany(company)));
 	}
 	
 	@PostMapping
-	public ResponseEntity<Complain> ingest(@RequestBody Complain complain){
-		return ResponseEntity.ok(this.complainService.ingest(complain));
+	public ResponseEntity<Response<Complain>> ingest(@Valid @RequestBody Complain complain, BindingResult result){
+		if(result.hasErrors()) {
+			List<String> errors = new ArrayList<String>();
+			result.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(new Response<Complain>(errors));
+		}
+		return ResponseEntity.ok(new Response<Complain>(this.complainService.ingest(complain)));
 		
 	}
 	
 	@PutMapping(path = "/{id}")
-	public ResponseEntity<Complain> update(@PathVariable(name = "id") String id, @RequestBody Complain complain){
+	public ResponseEntity<Response<Complain>> update(@PathVariable(name = "id") String id, @Valid @RequestBody Complain complain, BindingResult result){
+		if(result.hasErrors()) {
+			List<String> errors = new ArrayList<String>();
+			result.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(new Response<Complain>(errors));
+		}
 		complain.setId(id);
-		return ResponseEntity.ok(this.complainService.update(complain));
+		return ResponseEntity.ok(new Response<Complain>(this.complainService.update(complain)));
 	}
 	
 	@DeleteMapping(path ="/{id}")
